c975f70e1646bf07d4e65c4ff12a35be73353d29
Arthur Judd <amjudd315@gmail.com>
2021-12-10 10:36:24 -0700

    changed request to only include version name

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,16 +3,14 @@ import { GithubConfig, GithubVersion, Host } from "./ResourceModels";
 
 describe(injectResource, ()=>{
     it("should inject oliver_assertions", ()=>{
-        let resource = "oliver_assertions"
-        let props = {
-            version: "1.4.0"
-        }
+        let resource = "oliver_assertions";
+        let version = "1.4.0";
         let mockEl = {
             setAttribute: jest.fn()
         }
         document.createElement = jest.fn().mockReturnValue(mockEl);
         document.head.appendChild = jest.fn();
-        injectResource(resource,props);
+        injectResource(resource,version);
         expect(mockEl.setAttribute).toHaveBeenCalledWith("src","https://raw.githubusercontent.com/amj311/oliver-test/174f5e4/src/Assertions.js")
         expect(document.head.appendChild).toHaveBeenCalledWith(mockEl)
     })
--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,44 @@ import constructImportableUrl from "./UrlGenerator";
 
 const resources:ResourceLib = require("./resources.json")
 
-type LoadProps = {
-    version: string,
-    eager?: boolean
-}
+// type LoadProps = {
+//     version: string,
+//     eager?: boolean
+// }
 
-type ResourcesRequest = { [resource:string]: LoadProps }
+type ResourcesRequest = { [resource:string]: string }
 
 
 export function load(request: ResourcesRequest) {
-    for (let [res, props] of Object.entries(request)) {
-        injectResource(res,props);
+    for (let [res, version] of Object.entries(request)) {
+        injectResource(res,version);
     }
 }
 
-export function injectResource(resource: string, props:LoadProps): void {
+export function injectResource(resource: string, version:string): void {
     // get access url from library
     let res = resources[resource];
     if (!res) {
         throw new Error("Could not find resource: " + resource);
     }
-    let version = res.versions[props.version]; 
-    let url = constructImportableUrl(res.host_config, version);
+    let versionData = res.versions[version]; 
+    let url = constructImportableUrl(res.host_config, versionData);
 
     // inject import
-    injectImportElement(res.type, url, props);    
+    injectImportElement(res.type, url);    
 }
 
 
-const ImportCreators: { [type:string]:(url:string,props:LoadProps)=>void } = {
+const ImportCreators: { [type:string]:(url:string,props:any)=>void } = {
     js: injectJsImport,
     // css: parseGithubStFile,
 }
 
-function injectImportElement(type:ResourceType, url: string, props: LoadProps) {
+function injectImportElement(type:ResourceType, url: string, props?: any) {
     return ImportCreators[type](url,props);
 }
 
-function injectJsImport(url:string,props:LoadProps) {
+function injectJsImport(url:string,props:any) {
     let el = document.createElement("script");
     el.setAttribute("src",url)
     document.head.appendChild(el);
